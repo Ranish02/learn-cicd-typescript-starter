be8b56389f4342749231f03d4f1ebff708c35917
Ranish02 <usaranish@gmail.com>
2026-08-08 21:34:38 -0300

    demo testing

---
+++ .tests/api.test.ts
@@ -0,0 +1,11 @@
+import {describe , expect, test} from "vitest";
+import { getAPIKey} from "../src/api/auth";
+
+describe("API TESTS", ()=>{
+    test("get api key is defined",()=>{
+        expect(getAPIKey).toBeDefined();
+    })
+   test("Get API key is working",()=>{
+        expect(getAPIKey("testingheaderstring")).not.toBeNull();
+    })
+})
